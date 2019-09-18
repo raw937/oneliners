37b252747a0465293fe862d01a1fd0f6c54c05d0
Richard Allen White III <raw937@gmail.com>
2019-09-18 14:58:29 -0700

    Add files via upload

---
+++ bin/deinterleave.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if (( $# == 2 )) ; then
+
+    paste - - - - - - - - \
+    | tee \
+        >(cut -f 1-4 \
+        | tr "\t" "\n" \
+        > $1) \
+    | cut -f 5-8 \
+    | tr "\t" "\n" \
+    > $2
+
+else
+
+    echo -e "\
+${0##*/}
+GY140920
+
+Deinterleaves a FASTQ file of paired reads
+
+Usage:
+
+    cat <in.fastq> | deinterleave.sh <out1.fq> <out2.fq>
+    
+    zcat <in.fq.gz> | deinterleave.sh <out1.fq> <out2.fq>
+    
+    samtools bam2fq -ns singles.fq <in.bam> \\
+    | deinterleave.sh <out1.fq> <out2.fq>"
+
+fi
